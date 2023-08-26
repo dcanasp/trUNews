9f5e42cacfbebf7a3362355b5e79eacb96f53814
jsarmientopu <jsarmientopu@unal.edu.co>
2023-08-26 08:44:55 -0500

    script y limpieza

--- src/utils/createHash.ts
+++ src/utils/createHash.ts
@@ -2,15 +2,23 @@ import { hash,verify } from "argon2";
 import { logger, permaLogger } from "./logger";
 
 export async function hashPassword(password:string): Promise<string>{
-    permaLogger.log("debug",await hash(password))
-    return await hash(password);
-    //TODO errores y guardarlos en permaLog
+    
+    try {
+        const hashPassword = await hash(password);
+        permaLogger.log("debug",hashPassword);
+        return hashPassword;
+        
+    } catch (error) {
+        permaLogger.log("debug",error);
+        return "fail";
+    }
+
 } 
 export async function verifyHash(password:string, hash:string): Promise<boolean> {
 
     if (await verify(hash,password)) {
-        return true
+        return true;
     } else {
-        return false
+        return false;
     }
 }
