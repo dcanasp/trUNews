bd381363ebd9f5aefc80ca51f67243c930663bcd
david cañas <dcanasp@unal.edu.co>
2023-09-05 12:23:20 -0500

    prueba branch

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,6 @@ export class App {
     this.app.use(helmet());
     this.app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(specs));
   
-    // this.registerControllers();
     this.app.use(routes);//importa index por default
     this.loggerMiddleware()
     
