be63e7f8e7ce950d0797ac7f19cfd604e98a1cd4
Yenifer Mora <ymoras@unal.edu.co>
2023-09-05 23:45:58 -0500

    add fecade article, add delete and create services

--- src/articles/article.controller.ts
+++ src/articles/article.controller.ts
@@ -1,11 +1,11 @@
-import { ArticleService } from './article.service';
+import { ArticleFacade } from './article.facade';
 import { Request, Response } from 'express';
 
 export class ArticleController {
-  constructor(private articleService: ArticleService) {}
+  constructor(private articleFacade: ArticleFacade) {}
 
-  public async getArticles(req: any, res: any) {
-    this.articleService.getArticles()
+  public async getArticles(req: Request, res: Response) {
+    this.articleFacade.getArticles(req)
       .then(articles => res.json(articles))
       .catch(err => {
         res.status(400).json(err);
@@ -13,8 +13,7 @@ export class ArticleController {
   }
 
   public getArticleById(req: Request, res: Response) {
-    const articleId = req.params.id;
-    this.articleService.getArticleById(articleId)
+    this.articleFacade.getArticleById(req)
       .then(article => res.json(article))
       .catch(err => {
         res.status(400).json(err);
@@ -22,31 +21,19 @@ export class ArticleController {
   }
 
   public createArticle(req: Request, res: Response, next: any) {
-    this.articleService.createArticle(req.body)
+    this.articleFacade.createArticle(req, req.body)
       .then(article => {
-        res.locals.newArticle = article; // Guarda el artículo en res.locals para pasarlo al siguiente middleware
-        next();
+        res.locals.newArticle = article; 
+        res.json(article);
       })
       .catch(err => {
         res.status(400).json(err);
       });
   }
 
-  public updateArticle(req: Request, res: Response) {
-    const articleId = req.params.id;
-
-    this.articleService.updateArticle(articleId, req.body)
-      .then(updatedArticle => res.json(updatedArticle))
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  }
-
   public deleteArticle(req: Request, res: Response) {
-    const articleId = req.params.id;
-
-    this.articleService.deleteArticle(articleId)
-      .then(() => res.json({ message: 'Article deleted successfully' }))
+    this.articleFacade.deleteArticle(req)
+      .then(() => res.json({ message: 'Artículo eliminado exitosamente' }))
       .catch(err => {
         res.status(400).json(err);
       });
